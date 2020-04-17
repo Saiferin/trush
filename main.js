54cdc8ff99536b50cef2197099b73b97c3896ea9
Saiferin <54333751+Saiferin@users.noreply.github.com>
2020-04-17 17:48:08 +0300

    21627
    88

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	$.getJSON(
-    "https://docs.google.com/spreadsheets/feeds/list/1k2DWEXIioffZZWhDDsqm5bDkY9i1mBp6FboKaUdVYKI/od6/public/basic?alt=json",
+    "https://docs.google.com/spreadsheets/feeds/list/1k2DWEXIioffZZWhDDsqm5bDkY9i1mBp6FboKaUdVYKI/od6/public/value?alt=json",
     function(data){
     console.log(data);
 	})
